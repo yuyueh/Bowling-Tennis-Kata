d6c5313a3ca6fe1cc743bc700f7a41af5826af9e
ed chang <ed.chang@xuenn.com>
2018-06-19 17:47:36 +0800

    ThirtyAll

--- BowlingTennisKata/BowlingTennisKata.UnitTests/TennisTests.cs
+++ BowlingTennisKata/BowlingTennisKata.UnitTests/TennisTests.cs
@@ -124,5 +124,20 @@ namespace Tennis.UnitTests
             // Assert
             Assert.AreEqual(result, expected);
         }
+
+        [Test]
+        public void ThirtyAll()
+        {
+            // Arrange
+            var expected = "Thirty-All";
+
+            // Act
+            _target.GivenFirstPlayerScore(2);
+            _target.GivenSecondPlayerScore(2);
+            var result = _target.Score();
+
+            // Assert
+            Assert.AreEqual(result, expected);
+        }
     }
 }
