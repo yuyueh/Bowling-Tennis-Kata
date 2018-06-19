c181f8bf97ecf6f122be4335f12c7b13d1b78edb
ed chang <ed.chang@xuenn.com>
2018-06-19 11:29:23 +0800

    Implement to pass test case "Test_FifteenLove"

--- BowlingTennisKata/BowlingTennisKata/Tennis.cs
+++ BowlingTennisKata/BowlingTennisKata/Tennis.cs
@@ -2,14 +2,22 @@
 {
     public class Tennis
     {
+        private int _firstPlayerScore = 0;
+        private int _secondPlayerScore = 0;
+
         public string Score()
         {
+            if (_firstPlayerScore == 1 && _secondPlayerScore == 0)
+            {
+                return "Fifteen-Love";
+            }
+
             return "Love-All";
         }
 
         public void SetOnePlayerScore(int score)
         {
-            throw new System.NotImplementedException();
+            _firstPlayerScore = score;
         }
     }
 }
