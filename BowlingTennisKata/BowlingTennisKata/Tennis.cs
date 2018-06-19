551d72ec2e60f52fcf79f87ea4470f0153e4abb6
ed chang <ed.chang@xuenn.com>
2018-06-19 17:43:34 +0800

    Implement

--- BowlingTennisKata/BowlingTennisKata/Tennis.cs
+++ BowlingTennisKata/BowlingTennisKata/Tennis.cs
@@ -5,12 +5,14 @@ namespace Tennis
     public class Tennis
     {
         private int _firstPlayerScore;
+        private int _secondPlayerScore;
         private Dictionary<int, string> _scoreMapping;
 
         public Tennis()
         {
             _scoreMapping = new Dictionary<int, string>()
             {
+                {0, "Love"},
                 {1, "Fifteen"},
                 {2, "Thirty"},
                 {3, "Forty"}
@@ -19,9 +21,9 @@ namespace Tennis
 
         public string Score()
         {
-            if (_firstPlayerScore > 0)
+            if (_firstPlayerScore > 0 || _secondPlayerScore > 0)
             {
-                return $"{_scoreMapping[_firstPlayerScore]}-Love";
+                return $"{_scoreMapping[_firstPlayerScore]}-{_scoreMapping[_secondPlayerScore]}";
             }
 
             return "Love-All";
@@ -34,7 +36,7 @@ namespace Tennis
 
         public void GivenSecondPlayerScore(int score)
         {
-            throw new System.NotImplementedException();
+            _secondPlayerScore = score;
         }
     }
 }
