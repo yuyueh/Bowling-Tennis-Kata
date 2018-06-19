7898c9c13371b3d8e9dd23d37a7f6afdeac74a1c
ed chang <ed.chang@xuenn.com>
2018-06-19 11:40:39 +0800

    Implement to pass test case "Test_FortyLove"

--- BowlingTennisKata/BowlingTennisKata/Tennis.cs
+++ BowlingTennisKata/BowlingTennisKata/Tennis.cs
@@ -14,7 +14,8 @@ namespace Tennis
             {
                 {0, "Love"},
                 {1, "Fifteen"},
-                {2, "Thirty"}
+                {2, "Thirty"},
+                {3, "Forty"}
             };
         }
 
