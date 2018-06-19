682285b98f8305e2154dfe0d472398b5e27c8a2f
ed chang <ed.chang@xuenn.com>
2018-06-19 17:34:31 +0800

    Implement

--- BowlingTennisKata/BowlingTennisKata/Tennis.cs
+++ BowlingTennisKata/BowlingTennisKata/Tennis.cs
@@ -2,14 +2,20 @@
 {
     public class Tennis
     {
+        private int _firstPlayerScore;
+
         public string Score()
         {
+            if (_firstPlayerScore == 1)
+            {
+                return "Fifteen-Love";
+            }
             return "Love-All";
         }
 
         public void GivenFirstPlayerScore(int score)
         {
-            throw new System.NotImplementedException();
+            _firstPlayerScore = score;
         }
     }
 }
