6f4c6e81f9f5c97d150adaa9b3d075116c998f9d
ed chang <ed.chang@xuenn.com>
2018-06-27 11:13:02 +0800

    Refactor

--- BowlingTennisKata/BowlingTennisKata/Tennis.cs
+++ BowlingTennisKata/BowlingTennisKata/Tennis.cs
@@ -1,23 +1,26 @@
-﻿namespace Tennis
+﻿using System.Collections.Generic;
+
+namespace Tennis
 {
     public class Tennis
     {
         private int _firstPlayerScore;
+        private readonly Dictionary<int, string> _scoreMapper;
 
         public Tennis(string firstPlayer, string secondPlayer)
         {
+            _scoreMapper = new Dictionary<int, string>()
+            {
+                {1, "Fifteen"},
+                {2, "Thirty"},
+            };
         }
 
         public string Score()
         {
-            if (_firstPlayerScore == 1)
+            if (_firstPlayerScore > 0)
             {
-                return "Fifteen-Love";
-            }
-
-            if (_firstPlayerScore == 2)
-            {
-                return "Thirty-Love";
+                return _scoreMapper[_firstPlayerScore] + "-Love";
             }
 
             return "Love-All";
